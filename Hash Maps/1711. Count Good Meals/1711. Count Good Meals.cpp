f1b76189dce583e5243378488eaa449d02261372
Pr@ch!t@ <65648302+Sifa19@users.noreply.github.com>
2023-08-31 21:50:10 +0530

    Update 1711. Count Good Meals.cpp

--- Hash Maps/1711. Count Good Meals/1711. Count Good Meals.cpp
+++ Hash Maps/1711. Count Good Meals/1711. Count Good Meals.cpp
@@ -1,1 +1,34 @@
 
+class Solution {
+public:
+    int countPairs(vector<int>& deliciousness) {
+
+      long long count=0;
+
+	vector<int> powerOfTwoList(22);
+	for(int i=0;i<=21;i++){
+		powerOfTwoList[i]=pow(2,i);
+	}
+
+	map<int,int> frequecy;
+	for(int x:deliciousness){
+		frequecy[x]++;
+	}
+
+	for(int x:deliciousness){
+        //so that x shouldnt make a pair with itself
+		frequecy[x]=frequecy[x]-1;
+        
+		for(int i=0;i<=21;i++){
+			int target = powerOfTwoList[i]-x;
+
+			if(frequecy.count(target)){
+				count=count+frequecy[target];
+			}
+		}
+	}
+
+	return count % 1000000007;
+        
+    }
+};
